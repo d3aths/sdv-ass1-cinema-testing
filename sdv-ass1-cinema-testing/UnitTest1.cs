19e3c3cde5534f3ad8f115a290acc53c3e02e934
Celeste <celeste-quinn@live.nmit.ac.nz>
2022-08-15 13:43:17 +1200

    Add project files.

---
+++ sdv-ass1-cinema-testing/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace sdv_ass1_cinema_testing
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
